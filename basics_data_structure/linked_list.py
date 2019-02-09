27a2d88c09db80f43eecf53d5b9681aa5535b2a5
corenel <xxdsox@gmail.com>
2019-02-09 22:49:31 +0800

    Update linked list
    - Add  function to remove node
    - Update tests

--- basics_data_structure/linked_list.py
+++ basics_data_structure/linked_list.py
@@ -82,6 +82,17 @@ class LinkedList:
         assert head is not None
         return head
 
+    def eliminate_circularity(self):
+        """
+        Remove circularity of linked list
+        """
+        curr = self.dummy_head.next
+        while curr is not None:
+            if curr.next == self.dummy_head.next:
+                curr.next = None
+                break
+            curr = curr.next
+
     def append(self, node):
         # initialize dummy head node
         if self.dummy_head.next is None:
@@ -113,6 +124,20 @@ class LinkedList:
         for val in val_list:
             self.append(self.generate_node(val))
 
+    def pop(self, index):
+        """
+        Pop item by index
+
+        :param index: index of node to pop
+        :type index: int
+        """
+        assert index >= 0
+        if self.singly:
+            remove_list_node(curr=self.at(index), prev=self.at(index - 1))
+        else:
+            remove_list_node(curr=self.at(index), prev=None if index != 0 else self.dummy_head)
+        self.eliminate_circularity()
+
     def reverse(self):
         """
         Reverse linked list
@@ -130,7 +155,8 @@ class LinkedList:
                 prev = head
                 head = temp
             self.dummy_head.next = prev
-            return prev
+            self.eliminate_circularity()
+            return self.get_head()
         else:
             curr = None
             while head is not None:
@@ -139,7 +165,8 @@ class LinkedList:
                 curr.next = curr.prev
                 curr.prev = head
             self.dummy_head.next = curr
-            return curr
+            self.eliminate_circularity()
+            return self.get_head()
 
     def traverse(self):
         """
@@ -156,11 +183,37 @@ class LinkedList:
             curr = curr.next
         return node_list
 
+    def at(self, index):
+        """
+        Get node by index
 
-def delete_list_node(curr, prev=None):
-    if isinstance(curr, DListNode) and curr.prev is not None:
-        curr.prev.next = curr.next
-        del curr
+        :param index: index of node
+        :type index: int
+        :return: desired node
+        :rtype: ListNode or DListNone
+        """
+        if index < 0:
+            return self.dummy_head
+
+        head = self.get_head()
+        curr = head
+        cnt = 0
+        while curr is not None:
+            if cnt == index:
+                return curr
+            curr = curr.next
+            cnt += 1
+
+        raise IndexError
+
+
+def remove_list_node(curr, prev=None):
+    if isinstance(curr, DListNode):
+        if prev is None and curr.prev is not None:
+            prev = curr.prev
+        if prev is not None:
+            prev.next = curr.next
+            del curr
     elif isinstance(curr, ListNode) and prev is not None:
         prev.next = curr.next
         del curr
@@ -177,9 +230,22 @@ class TestLinkedList(unittest.TestCase):
         node_list.append_val_list(val_list)
         self.assertListEqual(val_list, node_list.to_list())
         # test reversing
+        val_list_reversed = val_list[::-1]
         node_list_reversed = LinkedList(singly=node_list.singly, head=node_list.reverse())
-        self.assertListEqual(val_list[::-1], node_list_reversed.to_list())
-        self.assertListEqual(val_list[::-1], node_list.to_list())
+        self.assertListEqual(val_list_reversed, node_list_reversed.to_list())
+        # test removing node
+        val_list_reversed.pop(2)
+        node_list_reversed.pop(2)
+        self.assertListEqual(val_list_reversed, node_list_reversed.to_list())
+        # test removing head node
+        val_list_reversed.pop(0)
+        node_list_reversed.pop(0)
+        self.assertListEqual(val_list_reversed, node_list_reversed.to_list())
+        # test removing tail node
+        node_list_reversed.pop(len(val_list_reversed) - 1)
+        val_list_reversed.pop(-1)
+        self.assertListEqual(val_list_reversed, node_list_reversed.to_list())
+        self.assertEqual(None, node_list_reversed.traverse()[-1].next)
 
     def test_doubly_linked_list(self):
         val_list = [1, 2, 3, 4, 5]
@@ -188,8 +254,22 @@ class TestLinkedList(unittest.TestCase):
         node_list.append_val_list(val_list)
         self.assertListEqual(val_list, node_list.to_list())
         # test reversing
+        val_list_reversed = val_list[::-1]
         node_list_reversed = LinkedList(singly=node_list.singly, head=node_list.reverse())
-        self.assertListEqual(val_list[::-1], node_list_reversed.to_list())
+        self.assertListEqual(val_list_reversed, node_list_reversed.to_list())
+        # test removing node
+        val_list_reversed.pop(2)
+        node_list_reversed.pop(2)
+        self.assertListEqual(val_list_reversed, node_list_reversed.to_list())
+        # test removing head node
+        val_list_reversed.pop(0)
+        node_list_reversed.pop(0)
+        self.assertListEqual(val_list_reversed, node_list_reversed.to_list())
+        # test removing tail node
+        node_list_reversed.pop(len(val_list_reversed) - 1)
+        val_list_reversed.pop(-1)
+        self.assertListEqual(val_list_reversed, node_list_reversed.to_list())
+        self.assertEqual(None, node_list_reversed.traverse()[-1].next)
 
 
 if __name__ == '__main__':
