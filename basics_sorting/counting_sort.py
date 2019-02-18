39e6e6c6a60323af5c40e80e6fc2ab2f66991b91
corenel <xxdsox@gmail.com>
2019-02-18 13:38:16 +0800

    Update counting sort

--- basics_sorting/counting_sort.py
+++ basics_sorting/counting_sort.py
@@ -7,7 +7,7 @@ Counting Sort
 
 import unittest
 
-from .util import generate_random_string
+from .util import generate_random_string, generate_random_array
 
 
 def counting_sort(array, num_max=256):
@@ -30,15 +30,35 @@ def counting_sort(array, num_max=256):
     :return: sorted array in ascending order
     :rtype: list[str]
     """
+
+    def to_idx(elem):
+        """
+        Convert element to index
+
+        :param elem: element in the input array
+        :type elem: int or str
+        :return: corresponding index of given element
+        :rtype: int
+        """
+        if isinstance(elem, str) and len(elem) == 1:
+            return ord(elem)
+        elif isinstance(elem, int):
+            return elem
+        else:
+            raise NotImplementedError
+
     # the output character array that will have sorted arr
-    output = ['' for _ in range(num_max)]
+    output = [None for _ in range(num_max)]
     # create a count array to store count of individual
     # characters and initialize count array as 0
     count = [0 for _ in range(num_max)]
 
+    # determine minimum value of input array
+    min_value = to_idx(min(array))
+
     # store count of each character
     for char in array:
-        count[ord(char)] += 1
+        count[to_idx(char) - min_value] += 1
 
     # change count[i] so that count[i] now contains actual
     # position of this character in output array
@@ -47,17 +67,27 @@ def counting_sort(array, num_max=256):
 
     # build the output character array
     for idx in range(len(array)):
-        output[count[ord(array[idx])] - 1] = array[idx]
-        count[ord(array[idx])] -= 1
+        output[count[to_idx(array[idx]) - min_value] - 1] = array[idx]
+        count[to_idx(array[idx]) - min_value] -= 1
 
     return output[:len(array)]
 
 
 class TestCountingSort(unittest.TestCase):
 
-    def test_counting_sort(self):
+    def test_counting_sort_string(self):
         for length in range(50, 100):
             val_list = generate_random_string(length)
+            self.assertListEqual(sorted(val_list), counting_sort(val_list))
+
+    def test_counting_sort_integer(self):
+        for length in range(10, 20):
+            val_list = generate_random_array(length)
+            self.assertListEqual(sorted(val_list), counting_sort(val_list))
+
+    def test_counting_sort_negative_integer(self):
+        for length in range(10, 20):
+            val_list = generate_random_array(length) + [-e for e in generate_random_array(length)]
             self.assertListEqual(sorted(val_list), counting_sort(val_list))
 
 
